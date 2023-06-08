fb1a38b69e5a4df091372d2dae4ad586d932a435
Christopher Baum <christopherabaum@gmail.com>
2023-06-08 11:22:44 -0500

    Install and import Router v5

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { Route, NavLink, Redirect, Switch } from 'react-router-dom';
+import Article from '../Article/Article';
+import Thumbnail from '../Thumbnail/Thumbnail';
 
 function App() {
   return (
     <div>
-      <h1>News Reader</h1>
+      <Switch>
+        <Route exact path='/'>
+          <h1>News Reader</h1>
+        </Route>
+
+        <Route path='/article/:id'>
+          <Article />
+        </Route>
+
+        <Route exact path='/thumbnail'>
+          <Thumbnail />
+        </Route>
+
+        <Redirect to='/' />
+      </Switch>
     </div>
   );
 }
