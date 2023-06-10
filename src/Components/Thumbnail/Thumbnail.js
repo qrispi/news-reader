01f26ed34014c7a5cb1c09801c177679b82b7030
Christopher Baum <christopherabaum@gmail.com>
2023-06-10 16:26:06 -0500

    Add image to thumbnails

--- src/Components/Thumbnail/Thumbnail.js
+++ src/Components/Thumbnail/Thumbnail.js
@@ -3,7 +3,8 @@ import './Thumbnail.css';
 function Thumbnail({data}) {
   return (
     <div className='thumbnail'>
-      <h2 className='thumb-title'>{data.title}</h2>
+        <img src={data.urlToImage} className='thumb-img' />
+        <h2 className='thumb-title'>{data.title}</h2>
     </div>
   );
 }
