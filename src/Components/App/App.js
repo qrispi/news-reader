95b5c2bf1e371ada76b98f324416febf4f954230
Christopher Baum <christopherabaum@gmail.com>
2023-06-10 15:41:08 -0500

    Import mock data and store in state in App with useEffect

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,27 +2,46 @@ import './App.css';
 import { Route, NavLink, Redirect, Switch } from 'react-router-dom';
 import Article from '../Article/Article';
 import Thumbnail from '../Thumbnail/Thumbnail';
+import { useState, useEffect } from 'react';
+import getTopStories from '../../api-calls';
 
 function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path='/'>
-          <h1>News Reader</h1>
-        </Route>
 
-        <Route path='/article/:id'>
-          <Article />
-        </Route>
+	const [topStories, setTopStories] = useState('');
+	const [getError, setGetError] = useState('');
 
-        <Route exact path='/thumbnail'>
-          <Thumbnail />
-        </Route>
+	useEffect(() => {
+		const promise = getTopStories();
+		console.log(promise)
+		promise.then(data => {
+			if (typeof data === 'string' || data instanceof String) {
+				setGetError(data);
+			} else {
+				setTopStories(data.articles)
+				console.log(data.articles);
+			}
+		});
+	}, []);
 
-        <Redirect to='/' />
-      </Switch>
-    </div>
-  );
+	return (
+		<div>
+			<Switch>
+				<Route exact path='/'>
+					<h1>News Reader</h1>
+				</Route>
+
+				<Route path='/article/:id'>
+					<Article />
+				</Route>
+
+				<Route exact path='/thumbnail'>
+					<Thumbnail />
+				</Route>
+
+				<Redirect to='/' />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
