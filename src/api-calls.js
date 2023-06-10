66132a3c26c52fb030978d4dec95895ce151a0d1
Christopher Baum <christopherabaum@gmail.com>
2023-06-10 15:02:22 -0500

    Add fetch function for top stories from api

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,3 +1,19 @@
 import 'dotenv/config'
 
-{process.env.API_KEY}
+const getTopStories = async() => {
+    const response = await fetch(
+        'https://newsapi.org/v2/top-headlines?country=us',
+        {
+			method: 'GET',
+			headers: {
+				'X-Api-Key': `${process.env.API_KEY}`,
+			}
+		}
+    );
+    if (!response.ok) {
+		throw new Error(`HTTP error! status: ${response.status}`);
+	}
+	return response
+}
+
+export default getTopStories
