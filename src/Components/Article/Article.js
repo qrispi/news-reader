a73dd1902a18d5eba6d999cf997c12cd0092d8a6
Christopher Baum <christopherabaum@gmail.com>
2023-06-11 15:46:58 -0500

    Add content to Article from props

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -1,9 +1,18 @@
 import './Article.css';
 
 function Article({data}) {
+
+    const date = new Date(data.publishedAt)
   return (
-    <div>
-      <h2>{data.title}</h2>
+    <div className='article-container'>
+        <div className='content-container'>
+            <img src={data.urlToImage} className='article-img' alt='article image'/>
+            <h2>{data.title}</h2>
+            <h3>{date.toUTCString()}</h3>
+            <h3>{data.author}</h3>
+            <p>{data.content}</p>
+            <a href={data.url}>Read More at {`${data.source.name}`} ➤</a>
+        </div>
     </div>
   );
 }
