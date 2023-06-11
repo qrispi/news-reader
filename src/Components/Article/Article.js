d3940159b32da2bcc36340f51b2a5928b245634a
Christopher Baum <christopherabaum@gmail.com>
2023-06-11 16:01:25 -0500

    Style Article

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -7,10 +7,10 @@ function Article({data}) {
     <div className='article-container'>
         <div className='content-container'>
             <img src={data.urlToImage} className='article-img' alt='article image'/>
-            <h2>{data.title}</h2>
+            <h2 className='article-title'>{data.title}</h2>
             <h3>{date.toUTCString()}</h3>
             <h3>{data.author}</h3>
-            <p>{data.content}</p>
+            <p className='article-text'>{data.content}</p>
             <a href={data.url}>Read More at {`${data.source.name}`} ➤</a>
         </div>
     </div>
