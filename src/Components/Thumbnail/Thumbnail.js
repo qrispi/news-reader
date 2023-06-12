70632d4bad17a2e77a05d4a3efc80c40f1a5da0e
Christopher Baum <christopherabaum@gmail.com>
2023-06-11 19:48:43 -0500

    Fix error of broken image URLs

--- src/Components/Thumbnail/Thumbnail.js
+++ src/Components/Thumbnail/Thumbnail.js
@@ -1,9 +1,10 @@
 import './Thumbnail.css';
 
 function Thumbnail({data}) {
+
   return (
     <div className='thumbnail'>
-        <img src={data.urlToImage} className='thumb-img' alt='thumbnail image'/>
+        <img onError={(e) => e.target.src = require('../../images/placeholder.png')} src={data.urlToImage ? data.urlToImage : require('../../images/placeholder.png')} className='thumb-img' alt='thumbnail image' />
         <h2 className='thumb-title'>{data.title}</h2>
     </div>
   );
